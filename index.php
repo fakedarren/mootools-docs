fb929014c4506bbd157cd41eeb59115efa5e46c8
fakedarren <darren.waddell@gmail.com>
2011-01-12 16:18:20 +0000

    Better?

--- index.php
+++ index.php
@@ -8,17 +8,19 @@ $section = $url[1];
 $type = $url[2];
 $method = $url[3];
 
-$html = markdown(file_get_contents("Docs/$section/$type/$method.md"));
+$path = "Docs/$section/$type/$method.md";
+ 
+$html = markdown(file_get_contents($path));
 
-function geshi($source){
+function geshi($matches){
+	$source = $matches[0];
 	$source = preg_replace('{<\/?pre>}', "", $source);
 	$source = preg_replace('{<\/?code>}', "", $source);
 	$geshi = new GeSHi($source, 'javascript');
 	$geshi->enable_classes();
 	return $geshi->parse_code();
 }
-
-$html = preg_replace_callback('{<pre[^>]*>([\s\S]*?)<\/pre>}', create_function('$matches','return geshi($matches[0]);') , $html);
+$html = preg_replace_callback('{<pre[^>]*>([\s\S]*?)<\/pre>}', "geshi", $html);
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -30,18 +32,18 @@ $html = preg_replace_callback('{<pre[^>]*>([\s\S]*?)<\/pre>}', create_function('
 </head>
 
 <body>
-
 	<div id="header">
 		<div>
 			<h1>MooTools</h1>
 		</div>
 	</div>
-
 	<div id="wrapper">
 		<div id="content">
 			<div id="leftcolumn">
 				<ul>
 					<li><h4><a href="/">Docs</a></h4></li>
+					<li><a href="/core/class/class">Class</a></li>
+					<li><a href="/core/element/element">Element</a></li>
 				</ul>
 			</div>
 			<div id="rightcolumn">
@@ -49,7 +51,5 @@ $html = preg_replace_callback('{<pre[^>]*>([\s\S]*?)<\/pre>}', create_function('
 			</div>
 		</div>
 	</div>
-
 </body>
 </html>
-
